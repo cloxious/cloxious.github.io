a22dd70ec1c3f09d419c2c113d3b839e5d9c38f7
David Sosa <sosadavid4280@icloud.com>
2024-06-12 14:17:59 -0600

    chore: Remove unused service worker files and update landing page design

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase, ref, onValue, set } from "firebase/database";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDNKToFbU511A_OF5y3l_9I22bqhG8rcsE",
+    authDomain: "cloxious-project.firebaseapp.com",
+    databaseURL: "https://cloxious-project-default-rtdb.firebaseio.com",
+    projectId: "cloxious-project",
+    storageBucket: "cloxious-project.appspot.com",
+    messagingSenderId: "44918807587",
+    appId: "1:44918807587:web:fb52ba0b5d209fa00ca596",
+    measurementId: "G-WWP6JL05PH"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const database = getDatabase(app);
+
+const counter = {
+    counter: 0
+};
+
+onValue(ref(database, 'counter'), (snapshot) => {
+    const data = snapshot.val();
+    counter.counter = data.counter;
+    document.querySelector('span').textContent = counter.counter;
+});
+
+document.querySelector('button').addEventListener('click', () => {
+    const button = document.querySelector('button');
+    const audio = new Audio('assets/clown.mp3');
+    audio.play();
+    button.disabled = true;
+    audio.addEventListener('ended', () => {
+        button.disabled = false;
+    });
+    set(ref(database, 'counter'), {
+        counter: counter.counter + 1
+    });
+});
