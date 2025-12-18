661d7cff8969fb73ea183fcb7006379f89a4976f
cloxious <sosadavid4280@outlook.com>
2025-12-17 20:49:42 -0600

    feat: integrate Tailwind CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   site: "https://cloxious.com",
   server: { port: 8080 },
+
   i18n: {
     locales: ["en", "es"],
     defaultLocale: "en",
     routing: { prefixDefaultLocale: true },
   },
+  vite: { plugins: [tailwindcss()] },
 });
