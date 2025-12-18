ead2d4442d500b28e9b820f5c62b7ac5649d3a49
cloxious <sosadavid4280@outlook.com>
2025-12-17 20:32:45 -0600

    feat: add internationalization support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,9 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://cloxious.com",
   server: { port: 8080 },
+  i18n: {
+    locales: ["en", "es"],
+    defaultLocale: "en",
+    routing: { prefixDefaultLocale: true },
+  },
 });
